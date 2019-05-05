b1d2849d53f8f3649c3def85fd3895e2bf37479d
Łukasz Kałużny <lukasz@kaluzny.pro>
2019-05-05 14:58:07 +0200

    fix etcd deployment

--- 05-bootstrapping-etcd.sh
+++ 05-bootstrapping-etcd.sh
@@ -5,14 +5,14 @@ for instance in controller-0 controller-1 controller-2; do
     -n ${instance}-pip --query "ipAddress" -otsv)
 
   scp -o StrictHostKeyChecking=no 05-script.sh kuberoot@${PUBLIC_IP_ADDRESS}:~/
-  ssh kuberoot@${PUBLIC_IP_ADDRESS} 'bash 05-script.sh' &
+  ssh kuberoot@${PUBLIC_IP_ADDRESS} 'bash 05-script.sh'
 done
 wait
 
 PUBLIC_IP_ADDRESS=$(az network public-ip show -g kubernetes \
     -n controller-0-pip --query "ipAddress" -otsv)
 
-ssh kuberoot@${PUBLIC_IP_ADDRESS} 'bash 05-script.sh sudo ETCDCTL_API=3 etcdctl member list \
+ssh kuberoot@${PUBLIC_IP_ADDRESS} 'sudo ETCDCTL_API=3 etcdctl member list \
   --endpoints=https://${INTERNAL_IP}:2379 \
   --cacert=/etc/etcd/ca.pem \
   --cert=/etc/etcd/kubernetes.pem \
